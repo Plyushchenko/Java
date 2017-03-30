40d9237e271665850eafaeeabfc61c7949cc7a5e
Daniil Plyushchenko <Plyushchenko@users.noreply.github.com>
2017-03-30 16:55:59 +0300

    Update MD5.java
    javadocs

--- src/main/java/MD5/MD5.java
+++ src/main/java/MD5/MD5.java
@@ -4,8 +4,18 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * interface for MD5
+ */
 public interface MD5 {
 
-    byte []getHash(Path path) throws IOException, NoSuchAlgorithmException;
+    /**
+     * builds MD5 hash
+     * @param path for path (file or folder)
+     * @return MD5 hash
+     * @throws IOException for unknown file system problem
+     * @throws NoSuchAlgorithmException for unknown digest problems
+     */
+    byte[] getHash(Path path) throws IOException, NoSuchAlgorithmException;
 
 }
