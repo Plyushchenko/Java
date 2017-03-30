38bad194a50f3aa05a03a218af0715646cdb4a90
Daniil Plyushchenko <Plyushchenko@users.noreply.github.com>
2017-03-30 16:57:17 +0300

    Update MD5SingleThread.java

--- src/main/java/MD5/MD5SingleThread.java
+++ src/main/java/MD5/MD5SingleThread.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
 public class MD5SingleThread implements MD5{
 
@@ -19,8 +18,10 @@ public class MD5SingleThread implements MD5{
         return getHashOfFile(path);
     }
 
+    /**
+     * read file in chuncks (1MB) and build MD5 hash
+     */
     byte[] getHashOfFile(Path path) throws IOException, NoSuchAlgorithmException {
-        System.out.println("  FILE: " + path);
         MessageDigest digest = MessageDigest.getInstance("MD5");
         DigestInputStream dis = new DigestInputStream(new FileInputStream(path.toString()), digest);
         byte[] buffer = new byte[1 << 20];
@@ -28,9 +29,10 @@ public class MD5SingleThread implements MD5{
         return digest.digest();
     }
 
-
+    /**
+     * build MD5 hash recursively for all subfolders and build hash of file for regular files
+     */
     private byte[] getHashOfFolder(Path path) throws NoSuchAlgorithmException, IOException {
-        System.out.println("FOLDER: + " + path);
         MessageDigest digest = MessageDigest.getInstance("MD5");
         digest.update(path.getFileName().toString().getBytes(StandardCharsets.UTF_8));
         File[] folderContent = new File(path.toString()).listFiles();
@@ -41,7 +43,6 @@ public class MD5SingleThread implements MD5{
                 } else {
                     digest.update(getHashOfFile(file.toPath()));
                 }
-                System.out.println(Arrays.toString(digest.digest()));
             }
         }
         return digest.digest();
